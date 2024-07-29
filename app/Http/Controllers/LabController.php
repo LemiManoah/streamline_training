809341ce47f14a355224e481fb00b812b93a6bb9
LemiManoah <lemi.manoah@gmail.com>
2024-07-29 19:19:56 +0300

    controller for the lab/lab_instruments

---
+++ app/Http/Controllers/LabController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Lab;
+
+class LabController extends Controller
+{
+    public function index(){
+        dd();
+    }
+}
